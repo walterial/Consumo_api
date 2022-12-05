f2c0d58203197893f5a1562dae8e7096baaa7d6c
walterial <waltumarin@gmail.com>
2022-12-05 11:50:07 -0500

    modificacndo pagina

--- pagina.php
+++ pagina.php
@@ -14,7 +14,7 @@ $datos = getdata();
 <head>
     <meta charset="UTF-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="style.css">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
@@ -38,8 +38,8 @@ $datos = getdata();
 
 
 
-<div class="row col  ">
-  <h2 class="text-center">Nasa Imagenes</h2>
+<div class="row col p-3">
+  <h2 class="text-center">NASA IMAGENES </h2>
   </div>
 
 <div class="row d-flex justify-content-center " >
@@ -77,7 +77,6 @@ $datos = getdata();
    </tbody>
 
 
-   
   
   </table>
 
